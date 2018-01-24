b9a7f020bfb0c2222db27c49b52489ae54da6f8b
bobanlukic <bbnlkc@gmail.com>
2018-01-24 20:10:59 +0100

    Initial commit, edited .gitignore file

---
+++ src/main/java/net/ltslab/nst/ordinacija/OrdinacijaApplication.java
@@ -0,0 +1,12 @@
+package net.ltslab.nst.ordinacija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdinacijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdinacijaApplication.class, args);
+	}
+}
---
+++ src/test/java/net/ltslab/nst/ordinacija/OrdinacijaApplicationTests.java
@@ -0,0 +1,17 @@
+package net.ltslab.nst.ordinacija;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdinacijaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+            
+	}
+        
+}
